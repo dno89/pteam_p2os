fd2fa26fc33585428a9aea9489be339ae535f87f
lafRA <chicca.ghidi@gmail.com>
2013-03-06 18:22:29 +0100

    Finiti i behavior MoveTowartGoal e TakeBall

--- src/behaviors/TakeTheBall.cpp
+++ src/behaviors/TakeTheBall.cpp
@@ -13,7 +13,7 @@
 using namespace pteam;
 
 
-TakeTheBall::TakeTheBall(float angle_threshold) : m_angle_threshold(angle_threshold) {
+TakeTheBall::TakeTheBall() {
   m_command_requested = false;
   m_gripper_down = false;
   m_take_ball = false;
@@ -38,14 +38,12 @@ pteam_p2os::RobotControlRequest TakeTheBall::operator() ( const pteam_p2os::Perc
   
   if(target_in_range) {
     Point2d ball_coord;
-    float ball_angle;
     float distance;
     high_resolution_clock::time_point current_time;
     high_resolution_clock::duration interval;
     
-    //leggiamo le coordinate della palla e ne calcoliamo l'angolo:
+    //leggiamo le coordinate della palla:
     ball_coord = ReadProperty<Point2d>("TARGET_POSITION");
-    ball_angle = RAD_TO_DEG(atan2(ball_coord.y, ball_coord.x));
        
       
     //valuto se la forca è abbassata o no
@@ -55,9 +53,10 @@ pteam_p2os::RobotControlRequest TakeTheBall::operator() ( const pteam_p2os::Perc
       distance = sqrt(pow(ball_coord.x, 2) + pow(ball_coord.y, 2));
 	
       if(distance < 0.2) {
-	//la palla è inforcata:
+	//la palla è sufficientemente vicina: la inforco e inizio ad alzare la forca sempre andando avanti:
 	m_start_time = high_resolution_clock::now();
 	  
+	//do il comando al gripper di alzare
 	req.gripper_move_down = false;
 	req.gripper_move_set = true;
 	  
@@ -65,10 +64,36 @@ pteam_p2os::RobotControlRequest TakeTheBall::operator() ( const pteam_p2os::Perc
 	m_take_ball = true;
       }
      } 
-     else {
-      if(!m_take_ball) {
-	//valuto se si sta abbassando o se devo ancora dare il comando
-	if(!m_command_requested) {
+     else {		//no il gripper non è giu
+      if(m_take_ball) {
+	//ho preso la palla controllo se la forca si è alzata: se si è alzata mi fermo
+	current_time = high_resolution_clock::now();
+	  
+	interval = current_time - m_start_time;
+	if(interval.count() > (4.5*10e-6)) {
+	  //il gripper si è abbassato:
+	  req.linear_speed = 0.0;
+	  req.linear_speed_set = true;
+	  
+	  ///HAPPY WIN =) SEEEEEE!!!	
+	}
+      } 
+      else {	//no ho preso la palla
+	//valuto se il gripper si sta abbassando o se devo ancora dare il comando
+	if(m_command_requested) {
+	  //ho gia dato il comando: contollo se la forca ha finito di scendere
+	  current_time = high_resolution_clock::now();
+	
+	  interval = current_time - m_start_time;
+	  if(interval.count() > (4.5*10e-6)) {
+	    //il gripper si è abbassato:
+	    req.linear_speed = 0.2;
+	    req.linear_speed_set = true; 
+	     
+	    m_gripper_down = true;  
+	  }
+	}
+	else {
 	  //il comando non è ancora stato dato
 	  m_start_time = high_resolution_clock::now();
 	
@@ -79,30 +104,8 @@ pteam_p2os::RobotControlRequest TakeTheBall::operator() ( const pteam_p2os::Perc
 	  //ci fermiamo
 	  req.linear_speed = 0.0;
 	  req.linear_speed_set = true;
-	 } 
-	 else {
-	  //ho gia dato il comando: contollo se la forca ha finito di scendere
-	  current_time = high_resolution_clock::now();
-	
-	  interval = current_time - m_start_time;
-	  if(interval.count() > (4.5*10e-6)) {
-	    //il gripper si è abbassato:
-	    req.linear_speed = 0.2;
-	    req.linear_speed_set = true; 
-	    
-	    m_gripper_down = true;
-	  }
-	}
-      } 
-      else {
-	//ho preso la palla controllo se la forca si è alzata: se si è alzata mi fermo
-	current_time = high_resolution_clock::now();
 	  
-	interval = current_time - m_start_time;
-	if(interval.count() > (4.5*10e-6)) {
-	  //il gripper si è abbassato:
-	  req.linear_speed = 0.0;
-	  req.linear_speed_set = true; 
+	  m_command_requested = true;
 	}
       }
     }
--- src/behaviors/TakeTheBall.h
+++ src/behaviors/TakeTheBall.h
@@ -19,13 +19,12 @@ using namespace std::chrono;
 namespace pteam {
 	
 class TakeTheBall : public CBehavior<pteam_p2os::Perception, pteam_p2os::RobotControlRequest> {
-  float m_angle_threshold;
-  bool m_command_requested;
-  bool m_gripper_down;
-  bool m_take_ball;
-  high_resolution_clock::time_point m_start_time;
+  bool m_command_requested;	//se è a true: ho richisto alla forca di abbassarsi, se è a false: non ho richiesto alla forca di abbassarsi
+  bool m_gripper_down;		//se  a true: la forca è giu se a false no
+  bool m_take_ball;		//se è a true la palla è inforcata se è a false no
+  high_resolution_clock::time_point m_start_time;	//istante iniziale del transitorio della forca
 public:
-	TakeTheBall(float angle_threshold = 2.0);
+	TakeTheBall();
         virtual pteam_p2os::RobotControlRequest operator() ( const pteam_p2os::Perception& in, bool* subsume = 0 );
         virtual ~TakeTheBall();
 };
